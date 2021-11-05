198bb22acb80db724aeeb28121201ec5c7c81762
Dobashi, Hiroki <hiroki.dobashi@gmail.com>
2021-11-06 02:39:37 +0900

    Sticky ScrollSpy Navigation

---
+++ assets/ananke/js/main.js
@@ -0,0 +1,29 @@
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("DOMContentLoad");
+
+  const tocActiveStateObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      if (entry.intersectionRatio > 0) {
+        console.log("aaaa");
+        clearActiveStatesInTableOfContents();
+
+        document
+          .querySelector(`nav li a[href="#${id}"]`)
+          .parentElement.classList.add("active");
+      }
+    });
+  });
+
+  document
+    .querySelectorAll("h1[id],h2[id],h3[id],h4[id]")
+    .forEach((section) => {
+      tocActiveStateObserver.observe(section);
+    });
+});
+
+function clearActiveStatesInTableOfContents() {
+  document.querySelectorAll("nav li").forEach((section) => {
+    section.classList.remove("active");
+  });
+}
